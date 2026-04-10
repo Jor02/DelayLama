1bc6b4ce324b589541cc21a83bc7a84aaf16e491
Jor02 <43887690+Jor02@users.noreply.github.com>
2026-04-10 08:05:40 +0200

    Add resources to CMake build and add resource definitions

---
+++ src/platform/Resource.h
@@ -0,0 +1,25 @@
+#pragma once
+
+// Precomposed background (360x510)
+#define IDB_BACKGROUND     130
+
+// Monk sprite sheet (1570x1866)
+#define IDB_MONK_SPRITES   131
+
+// Singing Y handle (10x10)
+#define IDB_HANDLE_Y       141
+
+// Singing X handle (10x10)
+#define IDB_HANDLE_X       142
+
+// Reverb 'Delay' handle (20x17)
+#define IDB_HANDLE_DELAY   151
+
+// Left 'Glide' knob sprite sheet (50x3000, 60 frames)
+#define IDB_KNOB_GLIDE     152
+
+// Right 'Voice' knob sprite sheet (50x3000, 60 frames)
+#define IDB_KNOB_VOICE     153
+
+// Splash/Help screen overlay (253x275)
+#define IDB_SPLASH         160
