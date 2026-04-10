20af44feba482a3a25033326165a97ef70458a40
Jor02 <43887690+Jor02@users.noreply.github.com>
2026-04-10 08:06:10 +0200

    add control class

---
+++ src/gui/controls/control.cpp
@@ -0,0 +1,9 @@
+#include "control.h"
+
+namespace DelayLama {
+namespace Controls {
+    void Control::setValue(float newValue) {
+        this->value = newValue;
+    }
+}
+}
---
+++ src/gui/controls/control.h
@@ -0,0 +1,10 @@
+namespace DelayLama {
+namespace Controls {
+    class Control {
+        public:
+            float value;
+        public:
+            void setValue(float newValue);
+    };
+}
+}
