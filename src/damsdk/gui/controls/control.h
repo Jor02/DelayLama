8ec5f2c4787ca006e58052dee9c0f4f46f996dde
Jor02 <43887690+Jor02@users.noreply.github.com>
2026-04-11 07:43:33 +0200

    Add constructor and members to Control class

--- src/damsdk/gui/controls/control.h
+++ src/damsdk/gui/controls/control.h
@@ -1,10 +1,31 @@
+#include "damsdk/gui/base/View.h"
+
+namespace DamSDK {
+    namespace Gui {
+        namespace Platform {
+            namespace Windows { class Bitmap; }
+        }
+    }
+}
+
 namespace DamSDK {
 namespace Gui {
 namespace Controls {
-    class Control {
+    class Control : public Base::View {
         public:
+            int parameterId;
+            float prevValue;
             float value;
+            float min;
+            float max;
+            float defaultValue;
+            float wheelSensitivity;
+            Platform::Windows::Bitmap *bitmap;
+            bool useAlphaBlending;
+            bool isEnabled;
+            void *unknown;
         public:
+            Control(RECT *pRect, void *unknown, int parameterId, Platform::Windows::Bitmap *bmp);
             void setValue(float newValue);
     };
 }
