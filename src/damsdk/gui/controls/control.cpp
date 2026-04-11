5e86a6094599c7234e9a987abc85f5e54e8e397b
Jor02 <43887690+Jor02@users.noreply.github.com>
2026-04-11 07:43:33 +0200

    Add constructor and members to Control class

--- src/damsdk/gui/controls/control.cpp
+++ src/damsdk/gui/controls/control.cpp
@@ -3,6 +3,24 @@
 namespace DamSDK {
 namespace Gui {
 namespace Controls {
+    Control::Control(RECT *pRect, void *unknown, int parameterId, Platform::Windows::Bitmap *bmp) : Base::View(pRect) {
+        this->unknown = unknown;
+        this->prevValue = 1.0;
+        this->max = 1.0;
+        this->parameterId = parameterId;
+        this->defaultValue = 0.5;
+        this->value = 0.0;
+        this->min = 0.0;
+        this->wheelSensitivity = 0.1;
+        this->bitmap = bmp;
+        this->useAlphaBlending = false;
+        this->isEnabled = true;
+
+        if (bmp != nullptr) {
+            View::useBitmap(bmp);
+        }
+    }
+
     void Control::setValue(float newValue) {
         this->value = newValue;
     }
