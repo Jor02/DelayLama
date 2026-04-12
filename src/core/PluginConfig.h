c82eb0f246cdfcfc0740208b96e49e087ecca4f9
Jor02 <43887690+Jor02@users.noreply.github.com>
2026-04-12 15:30:08 +0200

    move parameter definitions into pluginconfig

--- src/core/PluginConfig.h
+++ src/core/PluginConfig.h
@@ -1,2 +1,15 @@
 #define PRESET_COUNT 5
 #define PARAMETER_COUNT 4
+
+// Parameters
+#define LeftVoiceKnobParameterId 0
+#define SingingVerticalSliderParameterId 1
+#define ReverbSliderParameterId 2
+#define RightGlideKnobParameterId 3
+#define SingingHorizontalSliderParameterId 5
+#define MonkParameterId 6
+#define SingingEnabledParameterId 9
+#define TwoAxisSliderParameterId 7
+#define PitchValueParameterId 10
+#define VibratoAmountParameterId 11
+#define SplashScreenParameterId 12
